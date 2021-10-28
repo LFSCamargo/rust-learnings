72582db95bd5cffc282c604ad6728ba8777be63c
LFSCamargo <luizepauloxd@gmail.com>
2021-10-28 02:07:05 -0300

    chore: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+pub fn hello() -> String {
+  ("Hello, world!").to_string()
+}
+
 fn main() {
   println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::hello;
+
+  #[test]
+  fn test_main() {
+    assert_eq!(hello(), "Hello, world!");
+  }
 }
